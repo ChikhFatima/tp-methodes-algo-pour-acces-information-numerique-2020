8dd62a579bb91f66a16197b0e81237fe52fbe96b
FatimaCHIKH <fatima.chikh@etu.univ-paris-diderot.fr>
2020-03-14 13:33:55 +0100

    TP1 : Create Corpus

---
+++ MAAI/src/corpus/create/run/CreateCorpus.java
@@ -0,0 +1,24 @@
+package corpus.create.run;
+
+import corpus.factory.CorpusFactory;
+import corpus.model.MyFile;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class CreateCorpus {
+        public static final String PATHNAME = "ressources/";
+        public static final String FILENAME_SOURCE = "frwiki-debut.xml";
+        public static final String FILENAME_NEWCORPUS= "frwiki-corpus-2.xml";
+        public static final String TEXT="text";
+        public static final String CONTENT = "mathématique";
+        public static String ROOT_NEWCORPUS="frwiki";
+
+        public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+            CorpusFactory factory = new CorpusFactory();
+            MyFile newCorpus=factory.createCorpusFile(PATHNAME.concat(FILENAME_SOURCE),PATHNAME.concat(FILENAME_NEWCORPUS),ROOT_NEWCORPUS,CONTENT);
+            newCorpus.write();
+
+        }
+}
